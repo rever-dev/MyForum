ad890cc366dc05b6390f61595cc9132378f1a479
Rever <despairever@gmail.com>
2021-03-30 13:40:09 +0800

    no message

---
+++ app/src/main/java/com/rever/myforum/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rever.myforum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
